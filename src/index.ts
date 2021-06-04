9c82c6cef2781b4a284191a101d7b260ddb4fbc6
silva-nick <silva.d.nicholas@gmail.com>
2021-06-04 17:13:51 -0500

    start implementing lifecycle functions in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,44 @@
-import { Plugin, LoadContext } from '@docusaurus/types';
+import { LoadContext, OptionValidationContext } from '@docusaurus/types';
 import path from 'path';
+
+import { getUser, getRepos } from './api';
+import { repoOptions } from './types';
+
+export interface PluginOptions {
+  username: string;
+  repoOptions?: repoOptions;
+}
+
+export function validateOptions({
+  options,
+  validate,
+}: OptionValidationContext<PluginOptions>) {
+  const username = options.username;
+
+  if (!username) {
+    throw new Error('Expected a valid Github username.');
+  }
+
+  return options;
+}
+
+export default function plugin(context: LoadContext, options: PluginOptions) {
+  return {
+    name: 'docusaurus-portfolio',
+
+    async loadContent() {
+      let { username, repoOptions } = options;
+      repoOptions = repoOptions ? repoOptions : {};
+
+      const user = await getUser(username);
+      const repos = await getRepos(username, repoOptions);
+      return { user: user, repos: repos };
+    },
+
+    async contentLoaded({ content: userData, actions }) {
+      if (!userData) {
+        return;
+      }
+    },
+  };
+}
