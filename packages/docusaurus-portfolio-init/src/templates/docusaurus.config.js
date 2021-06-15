dea933fe2886c71f652d128aa9c1800d832cf16e
silva-nick <silva.d.nicholas@gmail.com>
2021-06-15 13:05:15 -0500

    add more init config/ theming based on docusaurus-init

---
+++ packages/docusaurus-portfolio-init/src/templates/docusaurus.config.js
@@ -0,0 +1,93 @@
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
+  title: 'My Portfolio',
+  tagline: 'A React-based static github website template.',
+  //url: '',
+  baseUrl: '/',
+  organizationName: '', // Usually your GitHub org/user name.
+  projectName: '', // Usually your repo name.
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  themeConfig: {
+    navbar: {
+      title: 'NavTitle',
+      logo: {
+        alt: 'My Logo',
+        src: 'img/logo.png',
+      },
+      items: [
+        {
+          to: '', //dir of content
+          activeBasePath: 'test',
+          label: 'Test',
+          position: 'left',
+        },
+        {
+          to: 'docs/',
+          activeBasePath: 'docs',
+          label: 'Docusaurus',
+          position: 'left',
+        },
+        { to: 'blog', label: 'Blog', position: 'left' },
+        {
+          href: 'https://github.com/silva-nick/docusaurus-portfolio',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'MyPortfolio',
+          items: [
+            {
+              label: 'Overview',
+              to: 'www.google.com',
+            },
+          ],
+        },
+        {
+          title: 'Guides',
+          items: [
+            {
+              label: 'Installation',
+              to: 'www.google.com',
+            },
+          ],
+        },
+      ],
+      // copyright: `Copyright © ${new Date().getFullYear()} MyPortfolio. Built with Docusaurus.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+    [
+      'docusaurus-portfolio',
+      {
+        //Config
+      },
+    ],
+  ],
+};
