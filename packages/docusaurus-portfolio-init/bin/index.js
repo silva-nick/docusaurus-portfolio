8e998965976278365f186ede3704479fd5699f5b
silva-nick <silva.d.nicholas@gmail.com>
2021-06-26 22:17:41 -0500

    add commander to init

---
+++ packages/docusaurus-portfolio-init/bin/index.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+/*
+    Original source: https://github.com/facebook/docusaurus/blob/master/packages/docusaurus-init/bin/index.js
+*/
+
+const chalk = require('chalk');
+const program = require('commander');
+const { default: init } = require('../lib');
+
+program
+  .version(require('../package.json').version)
+  .usage('<command> [options]');
+
+program
+  .command('init [siteName] [template] [username]')
+  .description('Initialize website with docusaurus-portfolio.')
+  .action((siteName = 'my-site', template = 'classic', username) => {
+    wrapCommand(init)(siteName, template, username);
+  });
+
+program.arguments('<command>').action((cmd) => {
+  program.outputHelp();
+  console.log(`  ${chalk.red(`\n  Unknown command ${chalk.yellow(cmd)}.`)}`);
+  console.log();
+});
+
+program.parse(process.argv);
+
+if (!process.argv.slice(2).length) {
+  program.outputHelp();
+}
